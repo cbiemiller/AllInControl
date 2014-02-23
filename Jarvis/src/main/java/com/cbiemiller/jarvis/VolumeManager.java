a5d478e1a2b0c11ad63fcc6bb22c854d4d114ebb
Casey Biemiller <caseybiemiller@gmail.com>
2014-02-23 16:06:17 -0700

    begin adding classes to interface with Android

--- Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
+++ Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
@@ -1,5 +1,19 @@
 package com.cbiemiller.jarvis;
 
-public class VolumeManager
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class VolumeManager extends Service
 {
+    /***
+     *
+     * Fill in this Service with an interface to Android AudioManager
+     * Leave learning methods to another Service.
+     *
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
