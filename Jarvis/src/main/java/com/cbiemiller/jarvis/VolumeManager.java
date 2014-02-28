1f90412d60d6fd27a46cc47e3c9438913e8762c2
cbiemiller <caseybiemiller@gmail.com>
2014-02-28 15:16:37 -0700

    Add Volume Stream Constants

--- Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
+++ Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
@@ -6,11 +6,17 @@ import android.media.AudioManager;
 import android.os.IBinder;
 import android.provider.Settings;
 
-public class VolumeManager extends Service
-{
+public class VolumeManager extends Service{
 	String DEP_BLUETOOTH_ON = Settings.Secure.BLUETOOTH_ON; //Deprecated in API 17
 	String BLUETOOTH_ON = Settings.Global.BLUETOOTH_ON; 
 	AudioManager am = null;
+	int STREAM_RING = AudioManager.STREAM_RING;
+	int STREAM_NOTIFICATION = AudioManager.STREAM_NOTIFICATION;
+	int STREAM_MEDIA = AudioManager.STREAM_MUSIC;
+	int STREAM_ALARM = AudioManager.STREAM_ALARM;
+	int STREAM_DTMF = AudioManager.STREAM_DTMF;
+	int STREAM_SYSTEM = AudioManager.STREAM_SYSTEM;
+	int STREAM_VOICE = AudioManager.STREAM_VOICE_CALL;
     /***
      *
      * Fill in this Service with an interface to Android AudioManager
