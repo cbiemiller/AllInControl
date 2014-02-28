8c8703a73d1bffa91a8c544883bcc3428a1cf695
cbiemiller <caseybiemiller@gmail.com>
2014-02-28 16:49:47 -0700

    Add static method to control volume at any time.

--- Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
+++ Jarvis/src/main/java/com/cbiemiller/jarvis/VolumeManager.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.content.Context;
 
 public class VolumeManager extends Service{
 	String DEP_BLUETOOTH_ON = Settings.Secure.BLUETOOTH_ON; //Deprecated in API 17
 	String BLUETOOTH_ON = Settings.Global.BLUETOOTH_ON; 
-	AudioManager am = null;
+	static AudioManager am = null;
 	int STREAM_RING = AudioManager.STREAM_RING;
 	int STREAM_NOTIFICATION = AudioManager.STREAM_NOTIFICATION;
 	int STREAM_MEDIA = AudioManager.STREAM_MUSIC;
@@ -29,5 +30,11 @@ public class VolumeManager extends Service{
     public IBinder onBind(Intent intent) {
         return null;
     }
+	
+	public static void setVolume(int stream, int volume_level,Context context)
+	{
+		am = (AudioManager) context.getSystemService(context.AUDIO_SERVICE);
+		am.setStreamVolume(stream,volume_level,0);
+	}
 
 }
